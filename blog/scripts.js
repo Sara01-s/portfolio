284ad7a4f07f669f8b2edc698c4e0ae32e25c50a
Sara01-s <saraosses01@gmail.com>
2025-06-05 23:10:12 -0400

    feat (portfolio): added games

--- blog/scripts.js
+++ blog/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', function() {
         const contentWrapper = document.querySelector('.content-wrapper');
         const isMobile = window.innerWidth <= 640;
-		
+
         if (!isMobile) {
             contentWrapper.scrollTop = 0
         }
